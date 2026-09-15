11db681c277f892ab14ffb39ecd92471e283f500
Naman Agrawal <144216560+Naman-the-Ace@users.noreply.github.com>
2026-09-15 13:42:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (19.9%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n];
-        for(auto it: flights) {
-            adj[it[0]].push_back({it[1], it[2]});  // {destination, price}
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto &it:f){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        
-        vector<int> dist(n, 1e9);
-        queue<pair<int,pair<int,int>>> q;  // {stops, {node, cost}}
-        
-        dist[src] = 0;
-        q.push({0, {src, 0}});  
-        
-        while(!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
-            
-            if(stops > k) continue;
-            
-            for(auto nbr: adj[node]) {
-                int adjNode = nbr.first;
-                int wt = nbr.second;  
-                
-                if(cost + wt < dist[adjNode] && stops <= k) {
-                    dist[adjNode] = cost + wt;
-                    q.push({stops + 1, {adjNode, cost + wt}});
-                }
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>pq;
+        pq.push({0,src,0});
+   // vector<int>dist(n,1e9);
+   vector<int>min_stops(n,INT_MAX);
+
+
+        while(!pq.empty()){
+            auto[cost,node,stops] =pq.top();
+            pq.pop();
+            if(node == dst) return cost;
+
+            if(stops>=min_stops[node]) continue;
+            min_stops[node]=stops;
+            if(stops>k) continue;
+
+            for(auto& [v,wt]:adj[node]){
+                    pq.push({cost+wt,v,stops+1});
             }
         }
-        
-        if(dist[dst] == 1e9) return -1;  
-        return dist[dst];
+        return -1;
     }
 };
