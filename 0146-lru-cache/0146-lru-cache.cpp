cbfbdf4c6caf19a0512c6ab5b848bb3af52bf56b
Naman Agrawal <144216560+Naman-the-Ace@users.noreply.github.com>
2026-09-17 08:51:11 +0530

    Time: 35 ms (55.76%), Space: 50.7 MB (59.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,39 +1,42 @@
 class LRUCache {
 public:
+list<int>dll; //key
+map<int,pair<list<int>::iterator,int>>mp; // key->(iterator to node(address),value)
 
-vector<pair<int,int>>cache;
+
 int n;
-    LRUCache(int cap) {
-        n=cap;
+
+    LRUCache(int capacity) {
+      n= capacity;  
     }
     
+    void makeRecentlyUsed(int key){
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first =dll.begin();
+    }
+
     int get(int key) {
-      for(int i =0;i<cache.size();i++){
-        if(cache[i].first ==key){
-            int val = cache[i].second;
-            pair<int,int> temp = cache[i];
-            cache.erase(cache.begin()+i);
-            cache.push_back(temp);
-            return val;
-        }
-      }
-      return -1;  
+        if(mp.find(key)==mp.end()) return -1;
+        makeRecentlyUsed(key);
+         return mp[key].second;
     }
     
     void put(int key, int value) {
-        for(int i =0;i<cache.size();i++){
-            if(cache[i].first==key){
-                cache.erase(cache.begin()+i);
-                cache.push_back({key,value});
-                return;
-            }
-        }
-        if(cache.size()==n){
-            cache.erase(cache.begin());
-            cache.push_back({key,value});
+        if(mp.find(key)!=mp.end()){
+            mp[key].second = value;
+            makeRecentlyUsed(key);
         }
         else{
-            cache.push_back({key,value});
+            dll.push_front(key);
+            mp[key] = {dll.begin(),value};
+            n--;
+        }
+        if(n<0){
+            int key_to_be_deleted  = dll.back();
+            mp.erase(key_to_be_deleted);
+            dll.pop_back();
+            n++;
         }
     }
 };
